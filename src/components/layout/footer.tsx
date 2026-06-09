1fd95a6ec739f7ca9382709b3b3a7834916f98e8
DivineDusk DraGon <divineduskdragon08@gmail.com>
2026-06-09 18:10:34 +0000

    not that big make it smaller

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,9 +1,9 @@
 
 export function Footer() {
   return (
-    <footer className="border-t border-border/40 py-10 md:py-12">
+    <footer className="border-t border-border/40 py-8 md:py-10">
       <div className="container flex flex-col items-center justify-center">
-        <p className="text-center text-xl md:text-2xl font-normal leading-loose text-muted-foreground font-sans tracking-tight">
+        <p className="text-center text-sm md:text-base font-normal text-muted-foreground font-sans tracking-tight">
           &copy; 2026 Dhriti Erusalagandi. All rights reserved.
         </p>
       </div>
